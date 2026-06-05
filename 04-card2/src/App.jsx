45b753c4289c0db09d5cdee4255f1000357b36f4
Rahat Khan <kmrejoantanjim@gmail.com>
2026-06-06 05:46:34 +0600

    apply grid for card2

---
+++ 04-card2/src/App.jsx
@@ -0,0 +1,25 @@
+import Card from "./components/Card"
+
+
+const App = () => {
+  return (
+    <>
+      {/* <Navbar /> */}
+
+      <section className="jobs-section">
+        <div className="cards-grid">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+
+        </div>
+      </section>
+
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default App
---
+++ 04-card2/src/components/Card.jsx
@@ -0,0 +1,16 @@
+
+
+const Card = () => {
+    return (
+        <div className='card-container'>
+            <div className="top">
+                <div className="status-payment">
+                    <p className="status">available</p>
+                    <p className="payment">$55/hr</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
